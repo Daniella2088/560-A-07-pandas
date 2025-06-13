297dab2062e0b001b795c31109d1d1e99ee8c035
Daniella2088 <daniboat@ad.unc.edu>
2025-06-13 00:48:37 -0400

    first_last_name

--- roster.py
+++ roster.py
@@ -2,6 +2,8 @@
 import pandas as pd
 roster = ["Bacot", "Davis", "Cadeau"]
 player = {'Last Name': roster,
-          }
-data = pd.DataFrame(roster)
+          "First Name": ['Armando', 'RJ', 'Elliot'],
+          'height': [83, 72, 73],
+          'weight': [240, 180, 180]}
+data = pd.DataFrame(player)
 print (data)
